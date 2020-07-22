48b2ca24e9fa35945f398d476eeb014c4a9541ab
Scott Andrews <andrewssc@vmware.com>
2020-07-21 22:32:18 -0400

    Prune go dependencies
    Signed-off-by: Scott Andrews <andrewssc@vmware.com>

--- hack/tools.go
+++ hack/tools.go
@@ -9,7 +9,6 @@ package tools
 
 import (
 	_ "k8s.io/code-generator"
-	_ "knative.dev/test-infra/scripts"
 
 	// codegen: hack/generate-knative.sh
 	_ "knative.dev/pkg/hack"
@@ -19,6 +18,5 @@ import (
 	_ "k8s.io/code-generator/cmd/defaulter-gen"
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
-	_ "k8s.io/kube-openapi/cmd/openapi-gen"
 	_ "knative.dev/pkg/codegen/cmd/injection-gen"
 )

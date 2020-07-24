cb8f725a47f0fa8e6326f5f515102c08b24b4d4c
Scott Andrews <andrewssc@vmware.com>
2020-07-24 15:43:14 -0400

    Report unit test coverage to codecov
    Add stub tests to packages that should have tests, but don't so that
    they are reported with 0% coverage.
    
    Signed-off-by: Scott Andrews <andrewssc@vmware.com>

---
+++ pkg/reconciler/servicebindingprep/controller_test.go
@@ -0,0 +1,14 @@
+/*
+Copyright 2020 VMware, Inc.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package servicebindingprep
+
+import (
+	"testing"
+)
+
+func TestStub(t *testing.T) {
+	// TODO add tests
+}
---
+++ pkg/reconciler/servicebindingprep/resources/secret_test.go
@@ -0,0 +1,14 @@
+/*
+Copyright 2020 VMware, Inc.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package resources
+
+import (
+	"testing"
+)
+
+func TestStub(t *testing.T) {
+	// TODO add tests
+}

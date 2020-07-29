2a015cd1ba686b0138272c9460aea1fce8cce082
Scott Andrews <andrewssc@vmware.com>
2020-07-29 18:24:06 -0400

    More tests
    Signed-off-by: Scott Andrews <andrewssc@vmware.com>

--- pkg/reconciler/servicebinding/resources/servicebindingprojection_test.go
+++ pkg/reconciler/servicebinding/resources/servicebindingprojection_test.go
@@ -7,8 +7,108 @@ package resources
 
 import (
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	servicebindingv1alpha2 "github.com/vmware-labs/service-bindings/pkg/apis/servicebinding/v1alpha2"
+	servicebindinginternalv1alpha2 "github.com/vmware-labs/service-bindings/pkg/apis/servicebindinginternal/v1alpha2"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"knative.dev/pkg/tracker"
 )
 
-func TestMakeServiceBindingProjections(t *testing.T) {
-	// TODO add tests
+func TestMakeServiceBindingProjection(t *testing.T) {
+	True := true
+	tests := []struct {
+		name        string
+		binding     *servicebindingv1alpha2.ServiceBinding
+		expected    *servicebindinginternalv1alpha2.ServiceBindingProjection
+		expectedErr bool
+	}{
+		{
+			name: "project binding",
+			binding: &servicebindingv1alpha2.ServiceBinding{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: "my-namespace",
+					Name:      "my-binding",
+				},
+				Spec: servicebindingv1alpha2.ServiceBindingSpec{
+					Name: "my-binding",
+					Application: &servicebindingv1alpha2.ApplicationReference{
+						Reference: tracker.Reference{
+							APIVersion: "apps/v1",
+							Kind:       "Deployment",
+							Name:       "my-app",
+						},
+					},
+					Env: []servicebindingv1alpha2.EnvVar{
+						{
+							Name: "MY_VAR",
+							Key:  "my-key",
+						},
+					},
+				},
+				Status: servicebindingv1alpha2.ServiceBindingStatus{
+					Binding: &corev1.LocalObjectReference{
+						Name: "my-secret",
+					},
+				},
+			},
+			expected: &servicebindinginternalv1alpha2.ServiceBindingProjection{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: "my-namespace",
+					Name:      "my-binding",
+					Labels: map[string]string{
+						"service.binding/servicebinding": "my-binding",
+					},
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							APIVersion:         "service.binding/v1alpha2",
+							Kind:               "ServiceBinding",
+							Name:               "my-binding",
+							Controller:         &True,
+							BlockOwnerDeletion: &True,
+						},
+					},
+				},
+				Spec: servicebindinginternalv1alpha2.ServiceBindingProjectionSpec{
+					Name: "my-binding",
+					Application: servicebindinginternalv1alpha2.ApplicationReference{
+						Reference: tracker.Reference{
+							APIVersion: "apps/v1",
+							Kind:       "Deployment",
+							Name:       "my-app",
+						},
+					},
+					Env: []servicebindinginternalv1alpha2.EnvVar{
+						{
+							Name: "MY_VAR",
+							Key:  "my-key",
+						},
+					},
+					Binding: corev1.LocalObjectReference{
+						Name: "my-secret",
+					},
+				},
+			},
+		},
+	}
+	for _, c := range tests {
+		t.Run(c.name, func(t *testing.T) {
+			binding := c.binding.DeepCopy()
+			actual, err := MakeServiceBindingProjection(c.binding)
+			if actualErr := err == nil; actualErr == c.expectedErr {
+				if c.expectedErr {
+					t.Errorf("%s: MakeServiceBindingProjection() expected error", c.name)
+				} else {
+					t.Errorf("%s: MakeServiceBindingProjection() expected no error, got %v", c.name, err)
+				}
+			}
+			if diff := cmp.Diff(c.expected, actual); diff != "" {
+				t.Errorf("%s: MakeServiceBindingProjection() (-expected, +actual): %s", c.name, diff)
+			}
+			if diff := cmp.Diff(c.binding, binding); diff != "" {
+				t.Errorf("%s: MakeServiceBindingProjection() unexpected binding mutation (-expected, +actual): %s", c.name, diff)
+			}
+		})
+	}
 }
--- pkg/resolver/serviceable_resolver_test.go
+++ pkg/resolver/serviceable_resolver_test.go
@@ -6,9 +6,203 @@ SPDX-License-Identifier: Apache-2.0
 package resolver
 
 import (
+	"context"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	duckv1alpha1 "github.com/vmware-labs/service-bindings/pkg/apis/duck/v1alpha1"
+	labsv1alpha1 "github.com/vmware-labs/service-bindings/pkg/apis/labs/v1alpha1"
+	servicebindingv1alpha2 "github.com/vmware-labs/service-bindings/pkg/apis/servicebinding/v1alpha2"
+	"github.com/vmware-labs/service-bindings/pkg/client/clientset/versioned/scheme"
+	"github.com/vmware-labs/service-bindings/pkg/client/injection/ducks/duck/v1alpha1/serviceable"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	fakedynamicclient "knative.dev/pkg/injection/clients/dynamicclient/fake"
+	"knative.dev/pkg/tracker"
 )
 
-func TestStub(t *testing.T) {
-	// TODO add tests
+func init() {
+	// Add types to scheme
+	appsv1.AddToScheme(scheme.Scheme)
+	duckv1alpha1.AddToScheme(scheme.Scheme)
+	labsv1alpha1.AddToScheme(scheme.Scheme)
+	servicebindingv1alpha2.AddToScheme(scheme.Scheme)
+}
+
+func TestNewServiceableResolver(t *testing.T) {
+	ctx, _ := fakedynamicclient.With(context.Background(), scheme.Scheme, []runtime.Object{}...)
+	ctx = serviceable.WithDuck(ctx)
+	_ = NewServiceableResolver(ctx, func(types.NamespacedName) {})
+}
+
+func TestServiceableResolver_ServiceableFromObjectReference(t *testing.T) {
+	tests := []struct {
+		name        string
+		seed        []runtime.Object
+		ref         *tracker.Reference
+		parent      interface{}
+		expected    *corev1.LocalObjectReference
+		expectedErr bool
+	}{
+		{
+			name:        "empty",
+			expectedErr: true,
+		},
+		{
+			name: "lookup serviceable",
+			seed: []runtime.Object{
+				&unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "bindings.labs.vmware.com/v1alpha1",
+						"kind":       "ProvisionedService",
+						"metadata": map[string]interface{}{
+							"namespace": "my-namespace",
+							"name":      "my-service",
+						},
+						"status": map[string]interface{}{
+							"binding": map[string]interface{}{
+								"name": "my-secret",
+							},
+						},
+					},
+				},
+			},
+			parent: &servicebindingv1alpha2.ServiceBinding{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: "my-namespace",
+					Name:      "my-binding",
+				},
+			},
+			ref: &tracker.Reference{
+				APIVersion: "bindings.labs.vmware.com/v1alpha1",
+				Kind:       "ProvisionedService",
+				Namespace:  "my-namespace",
+				Name:       "my-service",
+			},
+			expected: &corev1.LocalObjectReference{
+				Name: "my-secret",
+			},
+		},
+		{
+			name: "track error",
+			seed: []runtime.Object{
+				&unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "bindings.labs.vmware.com/v1alpha1",
+						"kind":       "ProvisionedService",
+						"metadata": map[string]interface{}{
+							"namespace": "my-namespace",
+							"name":      "my-service",
+						},
+						"status": map[string]interface{}{
+							"binding": map[string]interface{}{
+								"name": "my-secret",
+							},
+						},
+					},
+				},
+			},
+			parent: nil,
+			ref: &tracker.Reference{
+				APIVersion: "bindings.labs.vmware.com/v1alpha1",
+				Kind:       "ProvisionedService",
+				Namespace:  "my-namespace",
+				Name:       "my-service",
+			},
+			expectedErr: true,
+		},
+		{
+			name: "informer factory error",
+			seed: []runtime.Object{
+				&labsv1alpha1.ProvisionedService{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: "my-namespace",
+						Name:      "my-service",
+					},
+				},
+			},
+			parent: &servicebindingv1alpha2.ServiceBinding{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: "my-namespace",
+					Name:      "my-binding",
+				},
+			},
+			ref: &tracker.Reference{
+				APIVersion: "bindings.labs.vmware.com/v1alpha1",
+				Kind:       "ProvisionedService",
+				Namespace:  "my-namespace",
+				Name:       "my-service",
+			},
+			expectedErr: true,
+		},
+		{
+			name: "lookup error",
+			seed: []runtime.Object{},
+			parent: &servicebindingv1alpha2.ServiceBinding{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: "my-namespace",
+					Name:      "my-binding",
+				},
+			},
+			ref: &tracker.Reference{
+				APIVersion: "bindings.labs.vmware.com/v1alpha1",
+				Kind:       "ProvisionedService",
+				Namespace:  "my-namespace",
+				Name:       "my-service",
+			},
+			expectedErr: true,
+		},
+		// TODO duckv1alpha1.ServiceableType is always returned, even if the fields are nil
+		// {
+		// 	name: "not serviceable",
+		// 	seed: []runtime.Object{
+		// 		&unstructured.Unstructured{
+		// 			Object: map[string]interface{}{
+		// 				"apiVersion": "apps/v1",
+		// 				"kind":       "Deployment",
+		// 				"metadata": map[string]interface{}{
+		// 					"namespace": "my-namespace",
+		// 					"name":      "my-service",
+		// 				},
+		// 			},
+		// 		},
+		// 	},
+		// 	parent: &servicebindingv1alpha2.ServiceBinding{
+		// 		ObjectMeta: metav1.ObjectMeta{
+		// 			Namespace: "my-namespace",
+		// 			Name:      "my-binding",
+		// 		},
+		// 	},
+		// 	ref: &tracker.Reference{
+		// 		APIVersion: "apps/v1",
+		// 		Kind:       "Deployment",
+		// 		Namespace:  "my-namespace",
+		// 		Name:       "my-service",
+		// 	},
+		// 	expectedErr: true,
+		// },
+	}
+	for _, c := range tests {
+		t.Run(c.name, func(t *testing.T) {
+			ctx, _ := fakedynamicclient.With(context.Background(), scheme.Scheme, c.seed...)
+			ctx = serviceable.WithDuck(ctx)
+			r := NewServiceableResolver(ctx, func(types.NamespacedName) {})
+
+			actual, err := r.ServiceableFromObjectReference(c.ref, c.parent)
+			if actualErr := err == nil; actualErr == c.expectedErr {
+				if c.expectedErr {
+					t.Errorf("%s: ServiceableFromObjectReference() expected error", c.name)
+				} else {
+					t.Errorf("%s: ServiceableFromObjectReference() expected no error, got %v", c.name, err)
+				}
+			}
+			if diff := cmp.Diff(c.expected, actual); diff != "" {
+				t.Errorf("%s: ServiceableFromObjectReference() ref (-expected, +actual): %s", c.name, diff)
+			}
+		})
+	}
 }
